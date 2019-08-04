9fef912628d6e9f416226bcb3e1d30bdaa6dcda7
Lucius Mathematica <52294958+k1n4kut4@users.noreply.github.com>
2019-08-04 20:42:07 +0100

    posts.service.ts

--- src/app/services/posts.service.ts
+++ src/app/services/posts.service.ts
@@ -1,9 +1,29 @@
-import { Injectable } from '@angular/core';
+import { Injectable } from '@angular/core'; 
+import { HttpClient, HttpErrorResponse, HttpParams } from '@angular/common/http';
+
+import { Observable, throwError } from 'rxjs';
+import 'rxjs/add/operator/map'
+import { map, catchError } from 'rxjs/operators';  
 
 @Injectable({
   providedIn: 'root'
 })
 export class PostsService {
 
-  constructor() { }
+  private apiUrl = 'https://www.sitepoint.com/wp-json/wp/v2/';
+
+	constructor(private http: HttpClient) { }   
+
+	getPostsList(): Observable<any> {  
+		let response = this.http.get(this.apiUrl + 'posts?_embed=true').pipe(
+      map((res: Response) => res.json()));
+    return response;
+	}
+
+	getPost(slug: string): Observable<any> {  
+		let response = this.http.get(this.apiUrl + `posts?slug=${slug}`).pipe( 
+			map((res: Response) => res.json()));
+    return response;
+	} 
+
 }
