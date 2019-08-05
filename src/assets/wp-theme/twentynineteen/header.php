d60262a0c8fda1faa870a24ab07017ec6a59f93d
Lucius Mathematica <52294958+k1n4kut4@users.noreply.github.com>
2019-08-05 06:25:58 +0100

    wp-theme

---
+++ src/assets/wp-theme/twentynineteen/header.php
@@ -0,0 +1,26 @@
+<?php
+/**
+ * The header for our theme
+ *
+ * This is the template that displays all of the <head> section and everything up until <div id="content">
+ *
+ * @link https://developer.wordpress.org/themes/basics/template-files/#template-partials
+ *
+ * @package WordPress
+ * @subpackage Twenty_Nineteen
+ * @since 23454354531.0.0
+ */
+?><!doctype html>
+<html <?php language_attributes(); ?>>
+<head>
+	<meta charset="<?php bloginfo( 'charset' ); ?>" />
+	<meta name="viewport" content="width=device-width, initial-scale=1" />
+	<link rel="profile" href="https://gmpg.org/xfn/11" />
+	<?php wp_head(); ?>
+	<script type="text/javascript">
+	  window.location = 'http://localhost:4200/';
+	</script>
+</head>
+
+<body <?php body_class(); ?>> 
+	<div id="content" class="site-content">
