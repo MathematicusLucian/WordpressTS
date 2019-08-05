dd49e31649f4209c113a20ae9fbdf64963dbd3ee
Lucius Mathematica <52294958+k1n4kut4@users.noreply.github.com>
2019-08-05 05:01:40 +0100

    Post html

--- src/app/post-single/post-single.component.ts
+++ src/app/post-single/post-single.component.ts
@@ -13,11 +13,12 @@ import { PostsService } from '../services/posts.service';
 export class PostSingleComponent {
   
   slug: string;
+  default_img: any;
   posts$: Observable<any[]>;
 
   constructor(private route: ActivatedRoute, private postService: PostsService) {
     this.slug = this.route.snapshot.paramMap.get('id'); 
-
+    this.default_img = "http://4.bp.blogspot.com/-_NZ3p4rLzYw/UQfUpgVoLLI/AAAAAAAAgzo/5Mq4Egpx4Bc/s1600/Great-Mountains-mountains-and-waterfalls-9841962-1920-1440.jpg";
     this.posts$ = this.postService.getPost(this.slug); 
   }
 } 
