43e33a8edd0db2fd5b5db4f28efef05b1bab636f
Lucius Mathematica <52294958+k1n4kut4@users.noreply.github.com>
2019-08-05 04:33:39 +0100

    Posts List html tweaks

--- src/app/posts-list/posts-list.component.ts
+++ src/app/posts-list/posts-list.component.ts
@@ -11,11 +11,11 @@ import { PostsService } from '../services/posts.service';
 })
 export class PostsListComponent {
   
+  default_img: any;
   posts$: Observable<any[]>;
 
   constructor(private postService: PostsService) {
-    this.posts$ = this.postService.getPostsList();
-
-    console.log(this.posts$);
+    this.default_img = "http://4.bp.blogspot.com/-_NZ3p4rLzYw/UQfUpgVoLLI/AAAAAAAAgzo/5Mq4Egpx4Bc/s1600/Great-Mountains-mountains-and-waterfalls-9841962-1920-1440.jpg";
+    this.posts$ = this.postService.getPostsList(); 
   }
 } 
